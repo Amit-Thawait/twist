d936de7cf83040263ae6029f41a3ce70b10e188c
Amit Thawait <amit17thawait@gmail.com>
2017-06-26 12:20:07 +0530

    Users can now be invited to accounts

---
+++ app/mailers/invitation_mailer.rb
@@ -0,0 +1,11 @@
+class InvitationMailer < ApplicationMailer
+
+  def invite(invitation)
+    @invitation = invitation
+    mail(
+      to: invitation.email,
+      subject: "Invitation to join #{invitation.account.name} on Twist"
+    )
+  end
+
+end
